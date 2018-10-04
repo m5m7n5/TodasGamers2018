c5127b31145e9085f624d40c9e9ec85e611f6dc4
m5m7n5 <sporemaniaco@gmail.com>
2018-10-05 00:43:56 +0200

    [WIP] Trabajando en los buenos giros

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
@@ -6,53 +7,37 @@ public class PlayerController : MonoBehaviour
     public GameObject SpriteObject;
     public Animator SpriteAnimator;
 
-    private float _prevHorizontalAxis;
-    private float _prevVerticalAxis;
+    private float AnimationTransitionSpeed = 5;
+    private float delayedHorizontalIncrement = 1;
+    private float delayedVerticalIncrement = 1;
+
+    private Vector2 directionVector = new Vector2(1, -1);
 
     // Use this for initialization
     void Start()
     {
-        _prevHorizontalAxis = 1;
-        _prevVerticalAxis = -11;
     }
 
     // Update is called once per frame
     void Update()
     {
-        bool flipedChange = false;
-        if (_prevHorizontalAxis > 0 && Input.GetAxis("Horizontal") < 0)
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        float verticalAxis = Input.GetAxis("Vertical");
+        if (Math.Abs(horizontalAxis) > 0 || Math.Abs(verticalAxis) > 0)
         {
-            flipedChange = true;
-            _prevHorizontalAxis = -1;
-        }
-        else if (_prevHorizontalAxis < 0 && Input.GetAxis("Horizontal") > 0)
-        {
-            flipedChange = true;
-            _prevHorizontalAxis = 1;
-        }
-        else if (_prevVerticalAxis > 0 && Input.GetAxis("Vertical") < 0)
-        {
-            flipedChange = true;
-            _prevVerticalAxis = -1;
-        }
-        else if (_prevVerticalAxis < 0 && Input.GetAxis("Vertical") > 0)
-        {
-            flipedChange = true;
-            _prevVerticalAxis = 1;
+            directionVector = new Vector2(Math.Sign(horizontalAxis), Math.Sign(verticalAxis));
         }
 
-        if (flipedChange)
-        {
-            StartFlip();
-        }
+        delayedHorizontalIncrement = directionVector.x < 0
+            ? Math.Max(delayedHorizontalIncrement + Time.deltaTime * AnimationTransitionSpeed * directionVector.x, -1)
+            : Math.Min(delayedHorizontalIncrement + Time.deltaTime * AnimationTransitionSpeed * directionVector.x, 1);
+        delayedVerticalIncrement = directionVector.y < 0
+            ? Math.Max(delayedVerticalIncrement + Time.deltaTime * AnimationTransitionSpeed * directionVector.y, -1)
+            : Math.Min(delayedVerticalIncrement + Time.deltaTime * AnimationTransitionSpeed * directionVector.y, 1);
 
+        SpriteAnimator.SetFloat("Horizontal", delayedHorizontalIncrement);
+        SpriteAnimator.SetFloat("Vertical", delayedVerticalIncrement);
         transform.Translate(Speed * Time.deltaTime *
                             new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")));
-    }
-
-    //True right, false left
-    public void StartFlip()
-    {
-        SpriteAnimator.SetBool("OneSide", !SpriteAnimator.GetBool("OneSide"));
     }
 }
