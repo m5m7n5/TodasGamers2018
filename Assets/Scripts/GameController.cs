1ba6643affd8195afff07c29d8f9184166bdfdca
m5m7n5 <sporemaniaco@gmail.com>
2018-10-07 20:49:56 +0200

    [WO]

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace Assets.Scripts
 {
@@ -6,8 +7,12 @@ namespace Assets.Scripts
     {
         public PlayerController PlayerController;
         public float MaxHorror;
+        public SpawnController Spawner;
+        public float SpawnRatio;
+        public float SpawnRadius = 7;
 
         private static GameController _instance;
+        private float _timeCounter;
 
         public static GameController Instance
         {
@@ -17,6 +22,26 @@ namespace Assets.Scripts
         private void Start()
         {
             _instance = this;
+            _timeCounter = 0;
+        }
+
+        private void Update()
+        {
+            _timeCounter += Time.deltaTime;
+            if (_timeCounter >= SpawnRatio)
+            {
+                List<Vector3> positions = new List<Vector3>();
+                Vector2 v;
+                for (int i = 0; i < 7; i++)
+                {
+                    v = Random.insideUnitCircle * SpawnRadius;
+                    positions.Add(PlayerController.transform.position + new Vector3(v.x, 1.5f, v.y));
+                    Debug.Log(positions[positions.Count - 1]);
+                }
+
+                Spawner.Spawn(2, 3, 2, positions);
+                _timeCounter = 0;
+            }
         }
     }
 }
---
+++ Assets/Scripts/SpawnController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour
+{
+    public GameObject GhostPrefab;
+    public GameObject SkeletonPrefab;
+    public GameObject PumpkinPrefab;
+
+    public void Spawn(int ghost, int skeleton, int pumpkin, List<Vector3> positions)
+    {
+        int i = 0;
+        GameObject g;
+        for (; i < ghost; i++)
+        {
+            g = Instantiate(GhostPrefab);
+            g.transform.position = positions[i];
+        }
+
+        for (; i < ghost + skeleton; i++)
+        {
+            g = Instantiate(SkeletonPrefab);
+            g.transform.position = positions[i];
+        }
+
+        for (; i < ghost + skeleton + pumpkin; i++)
+        {
+            g = Instantiate(PumpkinPrefab);
+            g.transform.position = positions[i];
+        }
+    }
+}
